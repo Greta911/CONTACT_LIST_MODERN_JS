434fed2618cc3476e4067eec541a4b2976ded915
Greta911 <grema20012000@yahoo.it>
2026-06-15 17:52:23 +0200

    Articulation des components

---
+++ src/components/contactList/template.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function getTemplate() {
+  return `
+    <h1> Contact List </h1>
+    <ul class="contact-list"></ul>
+    <p>Ma Contact List</p>
+    `;
+}
---
+++ src/main.js
@@ -0,0 +1,8 @@
+
+import ContactList from "./components/contactList/ContactList";
+
+
+new ContactList({
+  el: "#app",
+  apiURL: "https://6a2fdceba7f8866418d53067.mockapi.io/"
+});
