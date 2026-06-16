5457c9a4506f88e8d0653de0ebe896e8f1a33575
Greta911 <grema20012000@yahoo.it>
2026-06-16 09:34:30 +0200

    Intégration du template des components

--- src/components/contact/template.js
+++ src/components/contact/template.js
@@ -2,6 +2,49 @@ import "./styles.css";
 
 export default function getTemplate(contact) {
   return `
-  <li>${contact.firstname} ${contact.lastname}</li>
-    `;
+  <td class="p-4">
+                <span class="${contact.isEditing ? "hidden" : ""}">${contact.firstname}</span>
+                <input
+                  type="text"
+                  class="input-firstname ${!contact.isEditing ? "hidden" : ""} mt-1 block w-full px-3 py-2 bg-gray-100 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  value="${contact.firstname}"
+                />
+              </td>
+    
+    <td class="p-4">
+                <span class="${contact.isEditing ? "hidden" : ""}">${contact.lastname}</span>
+                <input
+                  type="text"
+                  class="input-lastname ${!contact.isEditing ? "hidden" : ""} mt-1 block w-full px-3 py-2 bg-gray-100 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  value="${contact.lastname}"
+                />
+              </td>          
+    <td class="p-4">
+                <span class="${contact.isEditing ? "hidden" : ""}">${contact.email}</span>
+                <input
+                  type="text"
+                  class="input-email ${!contact.isEditing ? "hidden" : ""} mt-1 block w-full px-3 py-2 bg-gray-100 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  value="${contact.email}"
+                />
+              </td> 
+    <td class="p-4">
+                <div class="flex justify-end space-x-2">
+                  <button
+                    class="btn-check ${!contact.isEditing ? "hidden" : ""} bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md"
+                  >
+                    <i class="fa-solid fa-check"></i>
+                  </button>
+                  <button
+                    class="btn-edit ${contact.isEditing ? "hidden" : ""} bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded-md"
+                  >
+                    <i class="fa-solid fa-pen-to-square"></i>
+                  </button>
+                  <button
+                    class="btn-delete ${contact.isEditing ? "hidden" : ""} bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md"
+                  >
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
+                </div>
+              </td>
+              `;
 }
